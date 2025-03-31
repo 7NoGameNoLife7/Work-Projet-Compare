acfd23424752bcd1885c95e5aa0a6fd7ed78d526
Burdon Jordan <burdon.jordan@gmail.com>
2025-03-31 13:49:07 +0200

    Styling update

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,33 +1,39 @@
-interface Recipe  {
-    name: string;
-    url: string | null;
-    description: string | null; 
-    author: string | null;
-    ingredients: string[] | null;
-    method: string[] | null;
-  };
-  interface RecipeCardProps {
-    recipe: Recipe;
-  }
-  
-
-const RecipeCard = ({recipe}:RecipeCardProps) => {
-    const { name, url, description, author, ingredients, method} = recipe;
-    
-  return (
-    <div >
-            <h3>{name}</h3>
-            <p>{description}</p>
-            <img src= "https://picsum.photos/200/300" alt= {name} />
-            <p>{ingredients}</p>
-            <p>{method}</p>
-            <h4>Author: {author}</h4>
-            <a href="">{url}</a>
-            <button type = "button" >ComparisonArea</button>
-            
-    </div>
-  );
+interface Recipe {
+	name: string;
+	url: string;
+	description: string | null;
+	author: string | null;
+	ingredients: string[] | null;
+	method: string[] | null;
 }
+interface RecipeCardProps {
+	recipe: Recipe;
+}
+
+const RecipeCard = ({ recipe }: RecipeCardProps) => {
+	const { name, url, description, author, ingredients, method } = recipe;
+
+	return (
+		<div className="recipe-card">
+			<img src="https://picsum.photos/200/300" alt={name} />
+			<div className="recipe-info">
+				<a href={url} type="_blank">
+					<h3>{name}</h3>
+				</a>
+				<p>{description}</p>
+				<h4>- {author}</h4>
+				<div className="recipe-details">
+					<p>{ingredients}</p>
+					<p>{method}</p>
+				</div>
+				<div className="recipe-btns">
+					<button type="button">Details</button>
+					<button type="button">Compare</button>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default RecipeCard;
 export type { Recipe };
